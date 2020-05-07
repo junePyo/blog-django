0b5ff379bc5e70187802ef7cef40c8f3d3166f10
junePyo <junepyo.suh@gmail.com>
2020-05-07 22:47:56 +0900

    created a new users app for user registration window by importing UserCreationForm; created a new register.html template that extends blog app's base.html, which in turn loads boostrap static file from the static folder

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), # Home page becomes the blog page by leaving the url blank
+    path('register/', user_views.register, name='register'),
 ]
